cbf4b6aefa5824b60bdf3d80214b04254ad29750
Michael Hanke <michael.hanke@gmail.com>
2025-05-31 06:36:26 +0200

    Further maturation
    Advance git-annex submodule to include a fix for the windows test
    failures observed in
    https://github.com/psychoinformatics-de/git-annex-wheel/issues/5
    
    More robust fetching of version tags in github actions.
    
    A bit more documentation.

--- src/git_annex/__init__.py
+++ src/git_annex/__init__.py
@@ -4,6 +4,20 @@ import sys
 
 
 def cli():
+    """Emulate a symlink to a binary.
+
+    This script essentially calls the `git-annex` binary that is shipped
+    with the package, but using the `argv` list (including a potentially
+    different executable name) pass to the script itself.
+
+    It relies on the `executable` argument of `subprocess.run()` to achieve
+    this.
+
+    This approach provides alternative means for git-annex's installation
+    method with symlinks pointing to a single binary, and works on platforms
+    without symlink support, and also in packages that cannot represent
+    symlinks.
+    """
     exe_dir = op.dirname(__file__)
     exe = op.join(
         exe_dir,
@@ -23,7 +37,7 @@ def cli():
         # try flush here to trigger a BrokenPipeError
         # within the try-except block so we can handle it
         # (happens if the calling process closed stdout
-        # already
+        # already)
         sys.stdout.flush()
     except BrokenPipeError:
         # setting it to None prevents Python from trying to

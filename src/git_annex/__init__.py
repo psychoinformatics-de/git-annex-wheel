bd1ca609e3251b224349e95cbd58ba680da50a4f
Christopher J. Markiewicz <markiewicz@stanford.edu>
2025-05-31 15:31:34 -0400

    feat: Use execv to remove subprocess overhead

--- src/git_annex/__init__.py
+++ src/git_annex/__init__.py
@@ -1,5 +1,5 @@
+import os
 import os.path as op
-import subprocess
 import sys
 
 
@@ -18,30 +18,8 @@ def cli():
     without symlink support, and also in packages that cannot represent
     symlinks.
     """
-    exe_dir = op.dirname(__file__)
-    exe = op.join(
-        exe_dir,
-        op.basename(sys.argv[0]),
+    executable = op.join(
+        op.dirname(__file__),
+        f'git-annex{".exe" if sys.platform.startswith("win") else ""}',
     )
-    args = [exe] + sys.argv[1:]
-    try:
-        subprocess.run(
-            args,
-            executable=op.join(
-                exe_dir,
-                f'git-annex{".exe" if sys.platform.startswith("win") else ""}',
-            ),
-            shell=False,
-            check=True,
-        )
-        # try flush here to trigger a BrokenPipeError
-        # within the try-except block so we can handle it
-        # (happens if the calling process closed stdout
-        # already)
-        sys.stdout.flush()
-    except BrokenPipeError:
-        # setting it to None prevents Python from trying to
-        # flush again
-        sys.stdout = None
-    except subprocess.CalledProcessError as e:
-        sys.exit(e.returncode)
+    os.execv(executable, sys.argv)

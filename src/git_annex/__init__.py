1bafa096d039ff2462a9317290d79d75113ef855
Christopher J. Markiewicz <markiewicz@stanford.edu>
2025-05-31 15:55:15 -0400

    rf: Use shared-scripts to call execv with minimal indirection

--- src/git_annex/__init__.py
+++ src/git_annex/__init__.py
@@ -1,4 +1,3 @@
-import os
 import os.path as op
 import sys
 
@@ -8,21 +7,3 @@ def get_executable():
         op.dirname(__file__),
         f'git-annex{".exe" if sys.platform.startswith("win") else ""}',
     )
-
-
-def cli():
-    """Emulate a symlink to a binary.
-
-    This script essentially calls the `git-annex` binary that is shipped
-    with the package, but using the `argv` list (including a potentially
-    different executable name) pass to the script itself.
-
-    It relies on the `executable` argument of `subprocess.run()` to achieve
-    this.
-
-    This approach provides alternative means for git-annex's installation
-    method with symlinks pointing to a single binary, and works on platforms
-    without symlink support, and also in packages that cannot represent
-    symlinks.
-    """
-    os.execv(get_executable(), sys.argv)
